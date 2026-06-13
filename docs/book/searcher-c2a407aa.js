9cc02064223af6974d57c9d70c78eff65a27160a
wk91p <ahmedismael197743@gmail.com>
2026-06-13 22:17:57 +0300

    changes to book, i really forgot what i did after re-build it with mdbook

--- docs/book/searcher-c2a407aa.js
+++ docs/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ebfe53df.js',
+                path_to_root + 'searchindex-2fcbd9c5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
