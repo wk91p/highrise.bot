6c578f2039869fcaa0175a89448f92c39d1087d9
wk91p <ahmedismael197743@gmail.com>
2026-06-15 23:07:28 +0300

    updated book

--- docs/book/searcher-c2a407aa.js
+++ docs/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e94f0aea.js',
+                path_to_root + 'searchindex-e2eeab2d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
