5748636b0c5875295ee669d42acbe3bec3329cf5
wk91p <ahmedismael197743@gmail.com>
2026-06-16 02:13:11 +0300

    added verison check function

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const checkSdkVersion = require("./lib/versionCheck");
 const Highrise = require("./src/Client/Highrise");
 const { OutfitItem } = require("./src/Models/HelperModel");
 const HttpClient = require("./src/Networking/Core/HttpClient");
 const EmotesManager = require("./src/Tools/EmotesManager");
 const Logger = require("./src/Tools/Logger");
 const Validator = require("./src/Tools/Validator");
+
+(async() => {
+    await checkSdkVersion()
+})()
 
 exports.Highrise = Highrise
 exports.Validator = Validator
