2ac959b1a44ce4e81889fdeaf89e4269ffc249b0
wk91p <ahmedismael197743@gmail.com>
2026-06-15 22:37:34 +0300

    new book build version after adding changes

--- docs/book/searcher-c2a407aa.js
+++ docs/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d5c3f8eb.js',
+                path_to_root + 'searchindex-e94f0aea.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
